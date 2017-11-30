6c74144e1390514a51d5b8af7aa4ade71d0a3bfe
xiguang182 <xiguang182@gmail.com>
2017-11-30 16:38:19 -0500

    a few test for this

--- generalTest.js
+++ generalTest.js
@@ -1,18 +1,36 @@
-//requires mocha.js installed local or globally
+//requires mocha.js installed locally or globally
+
+console.log(this === global)
+let message = "root"
 describe('Test - Initializing', () => {
   beforeEach(done => {
     done();
   });
-
+  
+  let message = "Test function"
   describe('Test 1', () => {
     it('this test', function(done) {
+      let message = "thisTest function"
       function whatsThis() {
-        return this;  // The value of this is dependent on how the function is called
+        let message = "whatsThis Function"
+        return this.message;  // The value of this is dependent on how the function is called
       }
       let obj = {
-        a: "text"
+        message: "obj Object"
       }
-      console.log(whatsThis.apply(this))
+      let msg = whatsThis();
+      console.log("whatsThis() is: ", msg);
+      console.log("whatsThis.apply(this) is: ",whatsThis.apply(obj));
+      function logThis(){
+        console.log("Object.keys(this) is: ", Object.keys(this));
+        // console.log("this.test is: ", this.test);
+        // console.log("this._runnable is: ", this._runnable);
+      }
+
+      logThis();
+      logThis.apply(whatsThis);
+      logThis.apply(obj)
+      console.log(this === global)
       done()
     })
   });
--- selfInvodePromise.js
+++ selfInvodePromise.js
@@ -5,28 +5,28 @@ describe('Test - Initializing', () => {
   });
 
   describe('Test 1', () => {
-    // it('self invoke promise test', function(done) {
-    //   let selfInvodePromise = (count, max)=>{
-    //     return new Promise ((suc, err)=>{
-    //       console.log("looping", count)
-    //       count++
-    //       suc({count,max})
-    //     }).then(ret =>{
-    //       if(ret.count < ret.max){
-    //         return selfInvodePromise(ret.count, ret.max);
-    //       }else{
-    //         return "all done"
-    //       }
-    //     })
-    //   }
-    //   selfInvodePromise(0, 20).then(ret => {
-    //     console.log(ret)
-    //   })
-    //   console.log("finish")
-    //   done();
-    // })
+    it('self invoke promise test', function(done) {
+      let selfInvodePromise = (count, max)=>{
+        return new Promise ((suc, err)=>{
+          console.log("looping", count)
+          count++
+          suc({count,max})
+        }).then(ret =>{
+          if(ret.count < ret.max){
+            return selfInvodePromise(ret.count, ret.max);
+          }else{
+            return "all done"
+          }
+        })
+      }
+      selfInvodePromise(0, 20).then(ret => {
+        console.log(ret)
+      })
+      console.log("finish")
+      done();
+    })
 
-    it('self invoke promise test 2', function(done) {
+    it('self invoke promise test 2: loop given array', function(done) {
       let selfInvodePromise = (array, index = 0)=>{
         return new Promise ((suc, err)=>{
           console.log("looping", index, "value", array[index])
