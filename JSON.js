90fbcdcdd86d5ad6a4be272abf83db23eb899f7f
xiguang182 <xiguang182@gmail.com>
2017-12-19 13:38:12 -0500

    JSON.stringify usage

---
+++ JSON.js
@@ -0,0 +1,32 @@
+//requires mocha.js installed locally or globally
+describe('Test - Initializing', () => {
+  beforeEach(done => {
+    done();
+  });
+  describe('Test 1', () => {
+    it('test item 1', function(done) {
+      let testObj ={
+        key1: 'value1',
+        key2: 'value2',
+        Obj2: {
+          key3: 'value3',
+        }
+      }
+      let testObj2 ={
+        key1: 'value1',
+        key2: 'value2',
+        Obj2: {
+          key3: 'value3',
+          toJSON(){
+            return 'value4'
+          }
+        }
+      }
+      console.log(JSON.stringify(testObj))
+      console.log(JSON.stringify(testObj,null,2))
+      console.log(JSON.stringify(testObj2,null,4))
+      console.log(JSON.stringify(testObj,['key1','key2'],4))
+      done()
+    })
+  });
+});
